bbf848255c1338e9090bd9876044833e9cf64eb4
dhitaanggraini <77924392+dhitaanggraini@users.noreply.github.com>
2021-01-24 20:20:06 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,37 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+               <strong> Selamat datang kembali </strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Sistem Informasi Data Penduduk Desa Air Teluk Hessa</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/kantor.jpg"></img></p>
+                          <p class="label-title" align="center"><strong>DATA PENDUDUK</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman Sistem Informasi Pengolahan Data Penduduk Di Desa Air Teluk Hessa.<br>
+                            Jalan Lintas Sumatera, No.22, Air Batu, Kabupaten Asahan, Sumatera Utara, Kode Pos : 21272. <br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data penduduk setempat, <br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data penduduk.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang sedang login.<br><br><br>
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            Copyright &COPY; <?= date('Y')?> Kantor Kepala Desa Air Teluk Hessa |
+            <a href="#">Dokumentasi</a>
+            <a href="#">Peta Situs</a>
+            <a href="#">Support</a>
+        </div>
+    </div>
+</div>
